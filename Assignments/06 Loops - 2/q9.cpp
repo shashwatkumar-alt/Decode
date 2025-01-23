52720f8baa463a41a9b897e81b46ea32fef58aac
shashwatkumar-alt <shashwatsk1802@gmail.com>
2025-01-24 00:00:08 +0530

    completed loops -2 assignment

--- Assignments/06 Loops - 2/q9.cpp
+++ Assignments/06 Loops - 2/q9.cpp
@@ -2,17 +2,24 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+bool armstrong(int i){
+    int digit = 0,dup = i;
+    while(dup>0){
+        digit++;
+        dup /= 10;
+    }
+    int sum = 0;
+    dup = i;
+    while(dup>0){
+        sum += pow((dup%10),digit);
+        dup /= 10;
+    }
+    if(sum==i) return true;
+    else return false;
+}
 int main() { 
     for(int i = 1; i<=500;i++){
-        if(i<10){
-            cout<<i<<" ";
-        }
-        else if(i<100){
-            int 
-        }
-        else{
-
-        }
+        if(armstrong(i)) cout<<i<<" ";
     }
     return 0;
 }
