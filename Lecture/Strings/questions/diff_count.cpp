b6b6f0418e1a0c9d20c8aab5ea6ae3fca45491b0
shashwatkumar-alt <shashwatsk1802@gmail.com>
2025-03-25 19:52:41 +0530

    doing lecture

---
+++ Lecture/Strings/questions/diff_count.cpp
@@ -0,0 +1,17 @@
+// Input a string and return the number of times the neighbouring characters are different from each other.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() { 
+    string s;
+    getline(cin,s);
+    int count = 0;
+    for(int i = 0; i<s.size();i++){
+        if(){
+
+        }
+        
+    }
+    cout<<count;
+    return 0;
+}
