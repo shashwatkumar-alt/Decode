6b2406e73f44336d350b0eaba8b50d2be1b68468
shashwatkumar-alt <shashwatsk1802@gmail.com>
2025-07-11 23:35:11 +0530

    just completing the lectures

--- Lecture/Searching/Binary Search 2/rotated_array_search.cpp
+++ Lecture/Searching/Binary Search 2/rotated_array_search.cpp
@@ -9,6 +9,11 @@ int main() {
     for(int i = 0; i<n;i++){
         cin>>arr[i];
     }
+    if (n==2){
+        if(target == nums[0]) return 0;
+        else if(target == nums[1]) return 1;
+        else return -1;
+    }
     // first, finding pivot element.
     int pivot = -1;
     int lo = 0,hi = n-1;
@@ -25,6 +30,35 @@ int main() {
         else if(arr[mid]>arr[hi]) lo = mid +1;
         else hi = mid -1;
     }
-    
+    if(pivot == -1){                // already sorted, no rotation.
+        lo = 0;
+        hi = n-1;
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) lo = mid + 1;
+            else hi = mid - 1;
+        }
+    }
+    if(target>=nums[0] && target <= nums[pivot-1]){     // present in first part of the array.
+        lo = 0;
+        hi = pivot -1;
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) lo = mid + 1;
+            else hi = mid - 1;
+        }
+    }
+    else {
+        lo = pivot;
+        hi = n-1;
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2;
+            if(arr[mid] == target) return mid;
+            else if(arr[mid] < target) lo = mid + 1;
+            else hi = mid - 1;
+        }
+    }
     return 0;
 }
