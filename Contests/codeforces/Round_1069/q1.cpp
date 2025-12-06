16b001d8d4c75354a71e988f5f829d8c832d5869
shashwatkumar-alt <shashwatsk1802@gmail.com>
2025-12-06 14:44:47 +0530

    cf round 1069 whic I f*cked up

---
+++ Contests/codeforces/Round_1069/q1.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+typedef long long ll;
+typedef vector<int> vi;
+#define pb push_back
+typedef vector<ll> vll;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvll;
+typedef pair<int,int> pi;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<int>> vvi;
+typedef pair<ll,ll> pll;
+static const bool fastIO = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+    return true;
+}();
+void conquer(){
+    int n;
+    cin>>n;
+    vi arr(n);
+    for(int i = 0; i<n;i++) cin>>arr[i];
+    sort(arr.begin(),arr.end());
+    auto it = unique(arr.begin(),arr.end());
+    arr.erase(it,arr.end());
+    int ans = arr.size();
+    for(int i = 0; i<ans;i++){
+        if(ans<=arr[i]){
+            ans = arr[i];
+            break;
+        }
+    }
+    cout<<ans<<'\n';
+}
+int main(){
+    ll tc;
+    cin>>tc;
+    while(tc--){
+        conquer();
+    }
+}
