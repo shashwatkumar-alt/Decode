901116bc36802cf3efdab69b81061d8586a928e9
shashwatkumar-alt <shashwatsk1802@gmail.com>
2026-01-23 12:59:11 +0530

    CP sheet solving

---
+++ CP_31_Sheet/800_rating/Game_with_Integers.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+typedef long long ll;
+typedef vector<int> vi;
+#define pb push_back
+typedef vector<ll> vll;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvll;
+typedef pair<int,int> pi;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<int>> vvi;
+typedef pair<ll,ll> pll;
+static const bool fastIO = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+    return true;
+}();
+void conquer(){
+    int n;
+    cin>>n;
+    if(n%3==1|| n%3==2 ) cout<<"First"<<endl;
+    else cout<<"Second"<<endl;
+}
+int main(){
+    ll tc;
+    cin>>tc;
+    while(tc--){
+        conquer();
+    }
+}
