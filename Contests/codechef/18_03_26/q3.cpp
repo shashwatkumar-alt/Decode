c90718749c7877a3db513160b84413e10e96da0b
shashwatkumar-alt <shashwatsk1802@gmail.com>
2026-03-21 19:25:01 +0530

    trying to upsolve the codechef contest.

--- Contests/codechef/18_03_26/q3.cpp
+++ Contests/codechef/18_03_26/q3.cpp
@@ -19,7 +19,28 @@ static const bool fastIO = [](){
     return true;
 }();
 void conquer(){
-    ;
+    int n,k;
+    cin>>n>>k;
+    vi arr(n),pos;
+    for(int i = 0; i<n;i++){
+        cin>>arr[i];
+        if(arr[i]==1) pos.push_back(i+1);
+    }
+    int sz = pos.size();
+    if(pos.empty()) no;
+    else if(pos[0]-1>k || (n-1-pos[sz-1])>k) no;
+    else{
+        bool ye = true;
+        for(int i = 1; i<sz;i++){
+            if(pos[i]-pos[i-1]<=k) {
+                ye = false;
+                break;
+            }
+        }
+        if(ye) yes;
+        else no;
+    }
+
 }
 int main(){
     ll tc;
