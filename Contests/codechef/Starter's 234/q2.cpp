9fc92776a25df0027f237c577ae1b19d002c57c2
shashwatkumar-alt <shashwatsk1802@gmail.com>
2026-04-15 22:10:34 +0530

    solved only three question in codechef contest

---
+++ Contests/codechef/Starter's 234/q2.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+typedef long long ll;
+typedef vector<int> vi;
+#define pb push_back
+typedef vector<ll> vll;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvll;
+typedef pair<int,int> pi;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<int>> vvi;
+typedef pair<ll,ll> pll;
+static const bool fastIO = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+    return true;
+}();
+void conquer(){
+    int n;
+    cin>>n;
+    vi arr(n);
+    int ans = 0, count = 0;
+    for(int i = 0; i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr.rbegin(),arr.rend());
+    for(int i = 0; i<n; i++){
+        ans += arr[i];
+        if(ans<0) break;
+        count++;
+
+    }
+    cout<<count<<'\n';
+}
+int main(){
+    ll tc;
+    cin>>tc;
+    while(tc--){
+        conquer();
+    }
+}
