02674696f564704d4393174a8e831aa044fcaeb7
shashwatkumar-alt <shashwatsk1802@gmail.com>
2026-05-15 17:43:15 +0530

    code forces virtual contest. round 1076 (Div - 3) solved 2 . another face off. also, moved some files to another folder.

---
+++ Contests/CF_virtual/Div 3/Round_1076/q2.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+typedef long long ll;
+typedef vector<int> vi;
+#define pb push_back
+typedef vector<ll> vll;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvll;
+typedef pair<int,int> pi;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<int>> vvi;
+typedef pair<ll,ll> pll;
+static const bool fastIO = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+    return true;
+}();
+void conquer(){
+    int n;
+    cin>>n;
+    vi arr(n);
+    for(int i = 0; i < n; i++) cin>>arr[i];
+    for(int i = 0; i < n; i++){
+        if(arr[i] != n-i){
+            auto it = find(arr.begin()+(i+1),arr.end(),(n-i));
+            if (it != arr.end()) {
+                reverse(arr.begin() + i, it + 1); 
+            }
+            else reverse(arr.begin()+i,arr.end());
+            break;
+        }
+    }
+    for(int i = 0; i < n; i++) cout<<arr[i]<<" ";
+    cout<<'\n';
+}
+int main(){
+    ll tc;
+    cin>>tc;
+    while(tc--){
+        conquer();
+    }
+}
---
+++ Contests/CF_virtual/Div 3/Round_1076/q3.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+typedef long long ll;
+typedef vector<int> vi;
+#define pb push_back
+typedef vector<ll> vll;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvll;
+typedef pair<int,int> pi;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<int>> vvi;
+typedef pair<ll,ll> pll;
+static const bool fastIO = [](){
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+    return true;
+}();
+void conquer() {
+    int n, q;
+    cin>>n>>q;
+    vll a(n), b(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < n; i++) cin >> b[i];
+
+    a[n-1] = max(b[n-1], a[n-1]);
+    for(int i = n-2; i >= 0; i--) {
+        a[i] = max(a[i+1], b[i]);
+    }
+
+    vll pref(n + 1, 0);
+    pref[0] = 0;
+    for(int i = 0; i < n; i++) {
+        pref[i+1] = pref[i] + a[i];
+    }
+
+    for(int i = 0; i < q; i++) {
+        int l, r;
+        cin >> l >> r;
+        l--;
+        r--;
+        if(l >= 0 && r < n && l <= r) {
+            cout << pref[r+1] - pref[l] << " ";
+        }
+    }
+    cout << '\n';
+}
+int main(){
+    ll tc;
+    cin>>tc;
+    while(tc--){
+        conquer();
+    }
+}
